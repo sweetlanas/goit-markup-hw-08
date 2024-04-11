8830304d06b4fdf145e6be84c85336678c85677e
Svitlana Lazarieva <lazarevas3488@gmail.com>
2024-04-11 09:23:43 +0200

    add languages

---
+++ js/language.js
@@ -0,0 +1,13 @@
+function showDe() {
+    window.location.href = "index-de.html"; 
+}
+function showUk() {
+    window.location.href = "index.html";
+}
+
+function showDePortfolio() {
+    window.location.href = "portfolio-de.html";
+}
+function showUkPortfolio() {
+    window.location.href = "portfolio.html";
+}
